d131b590057a21ab82c7ea043c82b531ea93e867
shawlycode <orlandoshawly>
2021-06-18 20:36:43 +0000

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Title from './Title';
+import Description from './Description';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Title  title = 'Trioka Coffee'/>
+    <Description  description = 'Premium Gourmet coffee infused with herbs:Garnoderma, Ginseng and Tonkat ali '/>
+
+
+    </>
   );
 }
 
