d803b1dba0fadf5276a03563b573e0a7f4cdb12b
shawlycode <orlandoshawly>
2021-06-19 13:54:09 +0000

    add app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 
 import './App.css';
-import Title from './Title';
-import Description from './Description';
+import SimpleCard from './SimpleCard';
 
 
 function App() {
   return (
-    <>
-   <Title  title = 'Trioka Coffee'/>
-    <Description  description = 'Premium Gourmet coffee infused with herbs:Garnoderma, Ginseng and Tonkat ali '/>
-
-
-    </>
+    <SimpleCard />
   );
 }
 
